b326153499ecbb3f039037295b5489fc0a48928f
Oxyderkeya <bierna.sandrine@gmail.com>
2019-11-05 08:58:55 +0100

    Update project

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MovementController : MonoBehaviour
+{
+	public int horizontalRayCount;
+	public int verticalRayCount;
+	public LayerMask layerObstacle;
+
+	BoxCollider2D boxCollider;
+	Vector2 bottomLeft, bottomRight, topLeft, topRight;
+
+	float verticalRaySpacing;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		boxCollider = GetComponent<BoxCollider2D>();
+		verticalRaySpacing = boxCollider.bounds.size.y / (verticalRayCount - 1);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+	}
+
+	public void Move(Vector2 velocity)
+	{
+		CalculateBounds();
+		HorizontalMove(ref velocity);
+		transform.Translate(velocity);
+	}
+
+	void HorizontalMove(ref Vector2 velocity)
+	{
+		// XXX brique sort du mur, reassign valeur de distance dans le boucle
+
+		float direction = Mathf.Sign(velocity.x);
+		float distance = Mathf.Abs(velocity.x);
+
+		for(int i = 0; i < verticalRayCount; i++)
+		{
+			Vector2 baseOrigin = direction == 1 ? bottomRight : bottomLeft;
+			Vector2 origin = baseOrigin + new Vector2(0, verticalRaySpacing * i);
+
+			Debug.DrawLine(origin, origin + new Vector2(direction * distance, 0));
+			RaycastHit2D hit = Physics2D.Raycast(
+				origin,
+				new Vector2(direction, 0),
+				distance,
+				layerObstacle
+				);
+
+			if (hit)
+			{
+				velocity.x = hit.distance * direction;
+			}
+		}
+	}
+
+	void CalculateBounds()
+	{
+		bottomLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+		bottomRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+		topLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
+		topRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementController))]
+public class Player : MonoBehaviour
+{
+	[Tooltip("Number of meter by second")]
+	public float speed;
+
+	Vector2 velocity = new Vector2();
+	MovementController movementController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		movementController = GetComponent<MovementController>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		int horizontal = 0;
+		int vertical = 0;
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			horizontal += 1;
+		}
+		if(Input.GetKey(KeyCode.Q))
+		{
+			horizontal -= 1;
+		}
+		if(Input.GetKey(KeyCode.Z))
+		{
+			vertical += 1;
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			vertical -= 1;
+		}
+
+		velocity = new Vector2(horizontal * speed, vertical * speed);
+		movementController.Move(velocity * Time.deltaTime);
+	}
+}
